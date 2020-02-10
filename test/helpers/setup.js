14664c64308c087fa5ab2ba78bf145da3ca98d69
Ivan Demidov <Scrum@list.ru>
2020-02-10 17:24:26 +0300

    test: perf call hooks

--- test/helpers/setup.js
+++ test/helpers/setup.js
@@ -1,11 +1,16 @@
+// ./test/_setup.js
 require('jsdom-global')();
 window.Date = Date;
-const hooks = require('require-extension-hooks')
 
 // Setup browser environment
-require('browser-env')()
+require('browser-env')();
+const hooks = require('require-extension-hooks');
+const Vue = require('vue');
+
+// Setup Vue.js to remove production tip
+Vue.config.productionTip = false;
 
 // Setup vue files to be processed by `require-extension-hooks-vue`
-hooks('vue').plugin('vue').push()
+hooks('vue').plugin('vue').push();
 // Setup vue and js files to be processed by `require-extension-hooks-babel`
-hooks(['vue', 'js']).plugin('babel').push()
+hooks(['vue', 'js']).exclude(({filename}) => filename.match(/\/node_modules\//)).plugin('babel').push();
